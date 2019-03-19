3487c231a7cea967fc5886cdd4f18370a8e007b2
avp <nakulparmar15@gmail.com>
2019-03-19 20:55:02 +0530

    second commit

--- q2.java
+++ q2.java
@@ -5,16 +5,20 @@ public class q2{
         if(a>c || b>d) return false;
         if(a==c && b==d) return true;
 
-        return sum_rec(a,a+b,c,d) || sum_rec(a+b,b,c,d);
+        return sum_rec(a,a+b,c,d) || sum_rec(a+b,b,c,d);//recur both paths
     }
 
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);  //Scanner input object 
         int a,b,c,d;
+        System.out.println("Enter a:");
         a=in.nextInt();
+        System.out.println("Enter b:");
         b=in.nextInt();
+        System.out.println("Enter c:");
         c=in.nextInt();
+        System.out.println("Enter d:");
         d=in.nextInt();
-        System.out.println(sum_rec(a,b,c,d));
+        System.out.println(sum_rec(a,b,c,d));//print true or false
     }
 }
